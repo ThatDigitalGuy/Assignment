8ae8c116fdbb706f5fb779b548d376de0e1b747d
rhysm <admin@rhysmason.uk>
2024-11-06 16:00:28 +0000

    Idea

--- Assignment_Hospital/Program.cs
+++ Assignment_Hospital/Program.cs
@@ -1,14 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text.Json;
 
 namespace Assignment_Hospital
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Patient PatientObj = new Patient();
+
+            PatientObj.CreatePatient("John", "Smith", "18/01/24", ["1 test ave", "Royston", "Barnsley", "South Yorkshire", "S71 4XX"]);
+        }
+
+        class Patient
+        {
+            int IdCounter = 0;
+            protected int PatientId { get; set; }
+            protected string PatientFirstName { get; set; }
+            protected string PatientLastName { get; set; }
+            protected string PatientDOB { get; set; }
+            protected string[] PatientAddress { get; set; }
+            protected string[] PatientMedicalHistory { get; set; }
+            protected List<string>[] PatientPreScriptions { get; set; }
+            protected List<string>[] PatientMedications { get; set; }
+            protected List<string>[] PatientNotes { get; set; }
+
+
+            // New Patient Object
+            internal void CreatePatient(string FirstName, string LastName, string DOB, string[] Address)
+            {
+                PatientId = IdCounter++;
+                PatientFirstName = FirstName;
+                PatientLastName = FirstName;
+                PatientDOB = DOB;
+                PatientAddress = Address;
+                PatientMedicalHistory = [];
+                PatientPreScriptions = [];
+                PatientMedications = [];
+
+                string test = "test";
+                string filename = $"patient.txt";
+
+                StreamWriter writer = new StreamWriter(filename);
+                writer.Write("Hello");
+                writer.Close();
+            }
         }
     }
 }
